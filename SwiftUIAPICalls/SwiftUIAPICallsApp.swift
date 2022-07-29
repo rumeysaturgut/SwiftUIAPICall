6e626b7399f003a5f13e3b5e88aa5e5c4e1f14c6
Rumeysa Turgut <rumeysa.turgut@kitapyurdu.com>
2022-07-29 15:56:35 +0300

    Initial Commit

---
+++ SwiftUIAPICalls/SwiftUIAPICallsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAPICallsApp.swift
+//  SwiftUIAPICalls
+//
+//  Created by Rumeysa Turgut on 29.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAPICallsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
